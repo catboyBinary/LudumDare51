0f9f147512bd02dbcfb17057b1519f9725db7cca
b1n4ry <b1n4ry@guren.ru>
2022-10-02 04:07:22 +0300

    funny fun pads platformer jump jump change gravity whoosh
    very fun and entertainment. the design is very human. very easy to use. let my good friend demonstrate.

--- scripts/platformer_player.gd
+++ scripts/platformer_player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = 500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*1.2
@@ -14,7 +14,7 @@ func _physics_process(delta):
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY*up_direction.y
 		
 	if Input.is_action_just_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		$AnimationPlayer.stop()
@@ -51,3 +51,12 @@ func _on_platformer_2d_visibility_changed() -> void:
 		$Camera2d.current = true
 	else:
 		$Camera2d.current = false
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("jump_pad"): velocity.y = JUMP_VELOCITY*area.get_parent().scale.y*-1.35
+	if area.is_in_group("gravity_pad"): 
+		gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*-1.2*area.get_parent().scale.y
+		up_direction = Vector2(0,area.get_parent().scale.y)
+		$AnimatedSprite2d.flip_v = bool(up_direction.y+1)
+		
