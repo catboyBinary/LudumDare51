d26e08cc27a55364b1e1bb5d3bce8d84c5d48586
belowdecent <a.kanturin@yandex.ru>
2022-10-03 23:17:43 +0300

    3d bitch

--- scenes/games/fps/fps_player.gd
+++ scenes/games/fps/fps_player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody3D
 
 @onready var camera: Camera3D = $Camera3d
-@export var speed: float = 5.0
-var xz_movement: Vector3 = Vector3.ZERO
+@export var speed: float = 10.0
+var xz: Vector3 = Vector3.ZERO
 var y_movement: Vector3 = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -19,10 +19,15 @@ func _physics_process(delta: float) -> void:
 	var z = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
 	var x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var direction = Vector3(x, 0, z).rotated(Vector3.UP, rotation.y)
-	xz_movement = xz_movement.lerp(direction * speed, 0.2)
+	xz = xz.lerp(direction * speed, 0.2)
 	if (is_on_floor()):
-		y_movement.y = Input.get_action_strength("jump") * 5
+		y_movement.y = Input.get_action_strength("jump") * 10
 	else:
-		y_movement.y -= delta * 5
-	velocity = xz_movement + y_movement
+		y_movement.y -= delta * 14
+		
+	velocity = xz + y_movement
+	var target_fov: float = 110
+	if x == 0 and z >= 0:
+		target_fov = 100
+	camera.fov = lerp(camera.fov, target_fov, 0.05)
 	move_and_slide()
